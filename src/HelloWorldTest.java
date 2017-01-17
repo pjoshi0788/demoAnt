8ce9340742caa0a823b6add644d7bd69e1af71a4
unknown <praveen.joshi@CLBLAP1068.BRILLIO.COM>
2017-01-18 02:34:15 +0530

    Updated Jacoco

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -1,8 +1,13 @@
+import static org.junit.Assert.assertEquals;
 public class HelloWorldTest extends junit.framework.TestCase {
 
     public void testNothing() {
     }
     
+	public void testWillAlwaysPass(){
+		assertEquals(4,4);
+	}
+	
     public void testWillAlwaysFail() {
         fail("An error message");
     }
